1084baa3a15a4c3d606e5700f9e33b523d55aa82
Mahmoud Embaby <mah.embaby@outlook.com>
2023-11-06 05:29:44 +0200

    Initial commit

---
+++ backend/src/main/java/com/example/doctorkom/DoctorkomApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctorkom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorkomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorkomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/doctorkom/DoctorkomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctorkom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorkomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
