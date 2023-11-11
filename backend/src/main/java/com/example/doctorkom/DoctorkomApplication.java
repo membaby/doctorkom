5cd7563906c84d7cfda2f28024e7719de5a104d3
MostafaGalal1 <mostafag649.mg@gmail.com>
2023-11-11 19:21:28 +0200

    SCRUM-87 adding DAO method for adding doctor or patient bonus: adding DAO method for deleting also adding and deleting are cascaded operations (adding patient means adding user and account too) Note any changes made to DB is global to us since we share same SQL server thanks to embaby

--- backend/src/main/java/com/example/doctorkom/DoctorkomApplication.java
+++ backend/src/main/java/com/example/doctorkom/DoctorkomApplication.java
@@ -1,7 +1,16 @@
 package com.example.doctorkom;
 
+import com.example.doctorkom.DAOs.AccountDAO;
+import com.example.doctorkom.DAOs.PatientDAO;
+import com.example.doctorkom.DAOs.UserDAO;
+import com.example.doctorkom.Entities.Account;
+import com.example.doctorkom.Entities.Patient;
+import com.example.doctorkom.Entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoctorkomApplication {
@@ -10,4 +19,24 @@ public class DoctorkomApplication {
 		SpringApplication.run(DoctorkomApplication.class, args);
 	}
 
+	@Bean
+	@Autowired
+	public CommandLineRunner commandLineRunner (PatientDAO patientDAO) {
+		return runner -> {
+//			comment: Create new account and user and attach it to a patient and by cascading adding a user and an account to db
+			Account account = new Account("mostafam.galal82@gmail.com", "Mostafa_Galal", "galal123");
+			User user = new User(account, "mostafa", "galal");
+			Patient patient = new Patient(user, "BOBA");
+
+			System.out.println(patient);
+			System.out.println("Now inserting patient");
+			patientDAO.insert(patient);
+			System.out.println("Inserted!!");
+
+			Thread.sleep(10000);
+			System.out.println("Now deleting inserted patient");
+			patientDAO.delete(patient.getId());
+			System.out.println("Deleted!!");
+		};
+	}
 }
---
+++ backend/src/main/java/com/example/doctorkom/Entities/User.java
@@ -0,0 +1,56 @@
+package com.example.doctorkom.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+enum Gender {
+    Male,
+    Female
+}
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "AccountId")
+    private Integer id;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "Birthdate")
+    private Date birthdate;
+
+    @Column(name = "Gender")
+    private Gender gender;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "MobilePhone")
+    private String mobilePhone;
+
+    @Column(name = "LandlinePhone")
+    private String landlinePhone;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "AccountId")
+    private Account account;
+
+    public User(Account account, String firstName, String lastName) {
+        this.account = account;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
