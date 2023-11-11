a0f70bce77ace4698fdf77a64039ee1480533c71
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-11 10:33:41 +0200

    SCRUM-114 Seperate endpoints for patient and doctor registration

--- backend/src/main/java/com/example/doctorkom/UserRegistration/SignupController.java
+++ backend/src/main/java/com/example/doctorkom/UserRegistration/SignupController.java
@@ -3,19 +3,31 @@ package com.example.doctorkom.UserRegistration;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/signup")
 public class SignupController {
 	
-	@PutMapping("/signup")
-	public String signup(@RequestBody SignupInfoDTO signupRequest) {
+	@PutMapping("/patient")
+	public String patientSignup(@RequestBody SignupInfoDTO signupRequest) {
 		String err = ""; //err is an error message. If there is no problem with the signup info then err will be empty.
 		return err;
 	}
+
+	@PutMapping("/doctor")
+	public String doctorSignup(@RequestBody DoctorSignupInfo signupInfo)
+	{
+		String err = ""; //err is an error message. If there is no problem with the signup info then err will be empty.
+		return err;
+	}
+
 }
 
 
 class SignupInfoDTO{
-	String email, username, firstName, lastName, password, address, landline, mobile, Birthdate;
-	int age;
-	byte type; //0 for patient, 1 for doctor
-	byte gender; //0 for male, 1 for female
+	public String email, username, firstName, lastName, password, address, landline, mobile, Birthdate;
+	public int age;
+	public byte gender; //0 for male, 1 for female
+}
+
+class DoctorSignupInfo extends SignupInfoDTO{
+	public String specialty, title;
 }
