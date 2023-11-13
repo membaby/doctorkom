1031840d5352c7a980d77808cb2c6f085c412440
Sama Zayed <96790595+samatarekzayed@users.noreply.github.com>
2023-11-13 17:56:47 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
+// export default App;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+// TODO: Optimization using Lazy React.
+
+import Navbar from './components/navbar';
+// import Footer from './components/footer';
+
+import Register from './components/register';
+import Homepage from './components/homepage';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return true ? (
+    <Router>
+      <div class="fixed-top">
+        <Navbar />
+      </div>
+      <div class="empty_block d-block w-100"></div>
+
+      <div class="mb-5">
+        <Routes>
+          <Route path='/' element={ <Homepage/> } />
+          
+          <Route path='/register' element={ <Register /> } />
+        </Routes>
+      </div>
+      
+      <div class="fixed-bottom">
+        {/* <Footer /> */}
+      </div>
+    </Router>
+     ):(<></>)
+
 }
 
 export default App;
+
