33436f6fb880fe6fef0a60708f71f4c106480ae7
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-21 12:53:06 +0200

    SCRUM-115 Change LoginReponse class to include the role and the full account details if login is successful

---
+++ backend/src/main/java/com/example/doctorkom/Login/LoginReponse.java
@@ -0,0 +1,40 @@
+package com.example.doctorkom.Login;
+
+import com.example.doctorkom.DTOs.DoctorDTO;
+import com.example.doctorkom.DTOs.PatientDTO;
+import com.example.doctorkom.Entities.Role;
+
+public class LoginReponse {
+    public boolean success;
+    public Role role;
+    public PatientDTO patient;
+    public DoctorDTO doctor;
+    // public SystemAdmin systemAdmin;
+    // public ClinicAdmin clinicAdmin;
+
+    public LoginReponse(boolean success)
+    {
+        this.success = success;
+    }
+    public LoginReponse(boolean success, Role role, PatientDTO patient){
+        this.success = success;
+        this.role = role;
+        this.patient = patient;
+    }
+    public LoginReponse(boolean success, Role role, DoctorDTO doctor){
+        this.success = success;
+        this.role = role;
+        this.doctor = doctor;
+    }
+    // public LoginReponse(boolean success, Role role, SystemAdmin systemAdmin){
+    //     this.success = success;
+    //     this.role = role;
+    //     this.systemAdmin = systemAdmin;
+    // }
+    // public LoginReponse(boolean success, Role role, ClinicAdmin clinicAdmin){
+    //     this.success = success;
+    //     this.role = role;
+    //     this.clinicAdmin = clinicAdmin;
+    // }
+
+}
