b21c6591e8012eaeabe7bacdbd4a8663982e2868
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-23 14:00:54 +0200

    SCRUM-115 Use ClinicAdmin and SystemAdmin Uses DTO and entity classes of clinic and system admins

--- backend/src/main/java/com/example/doctorkom/Login/LoginController.java
+++ backend/src/main/java/com/example/doctorkom/Login/LoginController.java
@@ -4,15 +4,21 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.doctorkom.DTOMappers.AccountDTOMapper;
+import com.example.doctorkom.DTOMappers.ClinicAdminDTOMapper;
 import com.example.doctorkom.DTOMappers.DoctorDTOMapper;
 import com.example.doctorkom.DTOMappers.PatientDTOMapper;
+import com.example.doctorkom.DTOMappers.SystemAdminDTOMapper;
 import com.example.doctorkom.DTOs.AccountDTO;
+import com.example.doctorkom.DTOs.ClinicAdminDTO;
 import com.example.doctorkom.DTOs.DoctorDTO;
 import com.example.doctorkom.DTOs.PatientDTO;
+import com.example.doctorkom.DTOs.SystemAdminDTO;
 import com.example.doctorkom.Entities.Account;
+import com.example.doctorkom.Entities.ClinicAdmin;
 import com.example.doctorkom.Entities.Doctor;
 import com.example.doctorkom.Entities.Patient;
 import com.example.doctorkom.Entities.Role;
+import com.example.doctorkom.Entities.SystemAdmin;
 
 @RestController
 public class LoginController {
@@ -31,16 +37,16 @@ public class LoginController {
             case PATIENT:
                 PatientDTO patientDTO = PatientDTOMapper.INSTANCE.toDTO(loginService.getPatientAccount(fullAccount));
                 return new LoginReponse(true, Role.PATIENT, patientDTO);
-
             case DOCTOR:
                 DoctorDTO doctorDTO = DoctorDTOMapper.INSTANCE.toDTO(loginService.getDoctorAccount(fullAccount));
                 return new LoginReponse(true, Role.DOCTOR, doctorDTO);
-            
             case SYSTEM_ADMIN:
-            
+                SystemAdminDTO systemAdminDTO = SystemAdminDTOMapper.INSTANCE.toDTO(loginService.getSystemAdmin(fullAccount));
+                return new LoginReponse(true, Role.SYSTEM_ADMIN, systemAdminDTO);
             default: //CLINIC_ADMIN
+                ClinicAdminDTO clinicAdminDTO = ClinicAdminDTOMapper.INSTANCE.toDTO(loginService.getClinicAdmin(fullAccount));
+                return new LoginReponse(true, Role.SYSTEM_ADMIN, clinicAdminDTO);
         }
-        
     }
 
     @PutMapping("/recover_password")
@@ -67,7 +73,13 @@ class DummyLoginService{
         return new Doctor();
     }
 
-    
+    public SystemAdmin getSystemAdmin(Account account){
+        return new SystemAdmin(0);
+    }
+
+    public ClinicAdmin getClinicAdmin(Account account){
+        return new ClinicAdmin();
+    }
 }
 
 class DummyPasswordRecoverer{
--- backend/src/main/java/com/example/doctorkom/Login/LoginReponse.java
+++ backend/src/main/java/com/example/doctorkom/Login/LoginReponse.java
@@ -1,7 +1,9 @@
 package com.example.doctorkom.Login;
 
+import com.example.doctorkom.DTOs.ClinicAdminDTO;
 import com.example.doctorkom.DTOs.DoctorDTO;
 import com.example.doctorkom.DTOs.PatientDTO;
+import com.example.doctorkom.DTOs.SystemAdminDTO;
 import com.example.doctorkom.Entities.Role;
 
 public class LoginReponse {
@@ -9,8 +11,8 @@ public class LoginReponse {
     public Role role;
     public PatientDTO patient;
     public DoctorDTO doctor;
-    // public SystemAdmin systemAdmin;
-    // public ClinicAdmin clinicAdmin;
+    public SystemAdminDTO systemAdmin;
+    public ClinicAdminDTO clinicAdmin;
 
     public LoginReponse(boolean success)
     {
@@ -26,15 +28,15 @@ public class LoginReponse {
         this.role = role;
         this.doctor = doctor;
     }
-    // public LoginReponse(boolean success, Role role, SystemAdmin systemAdmin){
-    //     this.success = success;
-    //     this.role = role;
-    //     this.systemAdmin = systemAdmin;
-    // }
-    // public LoginReponse(boolean success, Role role, ClinicAdmin clinicAdmin){
-    //     this.success = success;
-    //     this.role = role;
-    //     this.clinicAdmin = clinicAdmin;
-    // }
+    public LoginReponse(boolean success, Role role, SystemAdminDTO systemAdmin){
+        this.success = success;
+        this.role = role;
+        this.systemAdmin = systemAdmin;
+    }
+    public LoginReponse(boolean success, Role role, ClinicAdminDTO clinicAdmin){
+        this.success = success;
+        this.role = role;
+        this.clinicAdmin = clinicAdmin;
+    }
 
 }
