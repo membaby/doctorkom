d7b1972efc22c76706e4b483feb7dc17de075b06
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-25 17:01:02 +0200

    SCRUM-115 Add getters and setters and default constructor for deserialization

--- backend/src/main/java/com/example/doctorkom/Controllers/LoginResponse.java
+++ backend/src/main/java/com/example/doctorkom/Controllers/LoginResponse.java
@@ -3,6 +3,13 @@ package com.example.doctorkom.Controllers;
 import com.example.doctorkom.DTOs.*;
 import com.example.doctorkom.Entities.Role;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class LoginResponse {
     public boolean success;
     public Role role;
