34419ace30a4ec8b6e93fd9f0985558241d7a7a3
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-25 17:13:09 +0200

    SCRUM-112 Add getters and setters to all DTOs

--- backend/src/main/java/com/example/doctorkom/DTOs/SystemUserDTO.java
+++ backend/src/main/java/com/example/doctorkom/DTOs/SystemUserDTO.java
@@ -2,6 +2,11 @@ package com.example.doctorkom.DTOs;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class SystemUserDTO {
     public int id;
     public String firstName, lastName, address, mobile, landline, gender;
