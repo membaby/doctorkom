6fc973c174f2aa45b1f1d6f1006f5ede1e4bb3fd
Sama Zayed <96790595+samatarekzayed@users.noreply.github.com>
2023-11-26 20:17:53 +0200

    update

--- frontend/src/components/homepage/index.js
+++ frontend/src/components/homepage/index.js
@@ -1,9 +1,29 @@
+// import React from 'react';
+// import './styles.css';
+
+// const Homepage = () => {
+//   return (
+//     <>
+//    Putting your well-being first
+//     </>
+//   );
+// };
+
+// export default Homepage;
+
 import React from 'react';
 import './styles.css';
 
 const Homepage = () => {
   return (
     <>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+    
+      <h1 className="well-being-text">Putting your well-being first</h1>
+      <button type="button" class="btnn ">Search</button>
+
+
     </>
   );
 };
