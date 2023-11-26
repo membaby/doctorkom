fa546044227317aedf7b156063736a4ef0d45bfa
A-Talha <96351797+A-Talha@users.noreply.github.com>
2023-11-26 19:23:20 +0200

    added patient,doctor,clinic admin and system admin registration. added verification emails for them added a success email upon successful registration missing verification code table

--- backend/src/main/java/com/example/doctorkom/Services/Notifier/NotificationService.java
+++ backend/src/main/java/com/example/doctorkom/Services/Notifier/NotificationService.java
@@ -5,30 +5,66 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
-@Service
+@Component
 public class NotificationService {
-    @Autowired
-    private final JavaMailSender mailSender;
 
+    private final JavaMailSender mailSender;
+    @Autowired
     public NotificationService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
     public void SendPatientCreatedEmail(String email) throws MessagingException {
         Notification notification = new Notification();
         notification.setTo(email);
-        notification.setSubject("Patient Created");
-        notification.setContent("Your account has been created");
+        notification.setSubject("Account  Created");
+        notification.setContent(notification.AccountCreatedContent_Patient());
         Send(notification);
     }
-    public void Send_VerificationEmail(String email) throws MessagingException {
+    public void SendDoctorCreatedEmail(String email,String name) throws MessagingException {
+        Notification notification = new Notification();
+        notification.setTo(email);
+        notification.setSubject("Account Created");
+        notification.setContent(notification.AccountCreatedContent_Doctor(name));
+        Send(notification);
+    }
+    public void SendClinicAdminCreatedEmail(String email) throws MessagingException {
+        Notification notification = new Notification();
+        notification.setTo(email);
+        notification.setSubject("Account Created");
+        notification.setContent(notification.AccountCreatedContent_ClinicAdmin());
+        Send(notification);
+    }
+    public void SendSystemAdminCreatedEmail(String email) throws MessagingException {
+        Notification notification = new Notification();
+        notification.setTo(email);
+        notification.setSubject("Account Created");
+        notification.setContent(notification.AccountCreatedContent_SystemAdmin());
+        Send(notification);
+    }
+    public void VerificationEmail_PD(String email,String code) throws MessagingException {
+        code = code.replaceAll("(.{3})", "$1 ").trim();
         Notification notification = new Notification();
         notification.setTo(email);
         notification.setSubject("Verification Email");
-        //content will be edited later to send a link to verify the account
-        notification.setContent("Please verify your email address");
+        notification.setContent(notification.VerifyEmailContent_PD(code));
+        Send(notification);
+    }
+    public void VerificationEmail_ClinicAdmin(String email,String code,String formlink) throws MessagingException {
+        Notification notification = new Notification();
+        notification.setTo(email);
+        notification.setSubject("Verification Email");
+        notification.setContent(notification.VerifyEmailContent_ClinicAdmin(code,formlink));
+        Send(notification);
+    }
+    public void VerificationEmail_SystemAdmin(String email,String code,String formlink) throws MessagingException {
+        Notification notification = new Notification();
+        notification.setTo(email);
+        notification.setSubject("Verification Email");
+        notification.setContent(notification.VerifyEmailContent_SystemAdmin(code,formlink));
         Send(notification);
     }
     public void CustomEmail(String email, String subject, String content) throws MessagingException {
@@ -42,17 +78,13 @@ public class NotificationService {
     private void Send(Notification notification) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-
         helper.setTo(notification.getTo());
         helper.setSubject(notification.getSubject());
         helper.setText(notification.getContent());
         helper.setFrom("doctorkomegy@outlook.com");
 
-        // Add attachments if needed
-        // FileSystemResource file = new FileSystemResource(new File("path/to/attachment.txt"));
-        // helper.addAttachment("Attachment.txt", file);
-
         mailSender.send(mimeMessage);
     }
+
 
 }
--- backend/src/main/java/com/example/doctorkom/Services/Register_LogIn/RegistrationService.java
+++ backend/src/main/java/com/example/doctorkom/Services/Register_LogIn/RegistrationService.java
@@ -1,65 +1,121 @@
 package com.example.doctorkom.Services.Register_LogIn;
 
-import com.example.doctorkom.Entities.Account;
-import com.example.doctorkom.Repositories.AccountRepository;
-import com.example.doctorkom.Repositories.SystemUserRepository;
+import com.example.doctorkom.Entities.*;
 import com.example.doctorkom.Services.Notifier.NotificationService;
+import com.example.doctorkom.Services.RepositoryHandler.RepositoryHandler;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
-    @Autowired
     NotificationService notificationService;
+    RepositoryHandler repositoryHandler;
     @Autowired
-    AccountRepository accountRepository;
-    @Autowired
-    SystemUserRepository systemUserRepository;
-
-    @Autowired
-    public RegistrationService(NotificationService notificationService, AccountRepository accountRepository,SystemUserRepository systemUserRepository) {
+    public RegistrationService(NotificationService notificationService, RepositoryHandler repositoryHandler) {
         this.notificationService = notificationService;
-        this.accountRepository = accountRepository;
-        this.systemUserRepository = systemUserRepository;
+        this.repositoryHandler = repositoryHandler;
     }
-
-    public String register(Account account) {
+    public String register_Patient(Account account, Patient patient,SystemUser systemUser) throws MessagingException {
         //check if the user exists
-        Account userExists = accountRepository.findByEmail(account.getEmail());
-        if (userExists != null) {
-            return "User already exists";
+        boolean accountExists = repositoryHandler.AccountExists(account.getEmail(),account.getUsername());
+        if (accountExists) {
+            return "Account already exists";
         }
-        try {
-            //send verification email
-            notificationService.Send_VerificationEmail(account.getEmail());
-            return "Account Created waiting for verification";
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        //generate verification code from 100000 to 999999
+        String code = generateVerificationCode();
+        //send verification email
+        notificationService.VerificationEmail_PD(account.getEmail(),code);
+        //add account to database
+        repositoryHandler.createAccount(account);
+        repositoryHandler.createSystemUser(systemUser);
+        repositoryHandler.createPatient(patient);
+        //store verification code and account in database
+            return "";
+    }
+    public String register_Doctor(Account account, Doctor doctor, SystemUser systemUser) throws MessagingException {
+        //check if the user exists
+        boolean accountExists = repositoryHandler.AccountExists(account.getEmail(),account.getUsername());
+        if (accountExists) {
+            return "Account already exists";
         }
+        //generate verification code from 100000 to 999999
+        String code = generateVerificationCode();
+        //send verification email
+        notificationService.VerificationEmail_PD(account.getEmail(),code);
+        //add account to database
+        repositoryHandler.createAccount(account);
+        repositoryHandler.createSystemUser(systemUser);
+        repositoryHandler.createDoctor(doctor);
+        //store verification code and account in database
+        return "";
+    }
+    public String register_ClinicAdmin(Account account, ClinicAdmin clinicAdmin, String formlink) throws MessagingException {
+        //check if the user exists
+        boolean accountExists = repositoryHandler.AccountExists(account.getEmail(),account.getUsername());
+        if (accountExists) {
+            return "Account already exists";
+        }
+        //generate verification code from 100000 to 999999
+        String code = generateVerificationCode();
+        //send verification email
+        notificationService.VerificationEmail_ClinicAdmin(account.getEmail(),code,formlink);
+        //add account to database
+        repositoryHandler.createAccount(account);
+        repositoryHandler.createClinicAdmin(clinicAdmin);
+        //store verification code and account in database
+        return "";
+    }
+    public String register_SystemAdmin(Account account, SystemAdmin systemAdmin, String formlink) throws MessagingException {
+        //check if the user exists
+        boolean accountExists = repositoryHandler.AccountExists(account.getEmail(),account.getUsername());
+        if (accountExists) {
+            return "Account already exists";
+        }
+        //generate verification code from 100000 to 999999
+        String code = generateVerificationCode();
+        //send verification email
+        notificationService.VerificationEmail_SystemAdmin(account.getEmail(),code,formlink);
+        //add account to database
+        repositoryHandler.createAccount(account);
+        repositoryHandler.createSystemAdmin(systemAdmin);
+        //store verification code and account in database
+        return "";
     }
 
-    public String verify(String email) {
+    public String verify(Account account,String code) throws MessagingException {
         //verify the account
         boolean isValid = true;
         if (!isValid) {
             return "Invalid verification code";
         }
         else {
-            return "Account verified";
+            if (account.getRole() == Role.PATIENT) {
+                notificationService.SendPatientCreatedEmail(account.getEmail());
+            }
+            else if (account.getRole() == Role.DOCTOR) {
+                //get doctor name
+                SystemUser systemUser = repositoryHandler.findSystemUserById(account.getId());
+                String name = systemUser.getFirstName();
+                notificationService.SendDoctorCreatedEmail(account.getEmail(),name);
+            }
+            else if (account.getRole() == Role.CLINIC_ADMIN) {
+                notificationService.SendClinicAdminCreatedEmail(account.getEmail());
+            }
+            else if (account.getRole() == Role.SYSTEM_ADMIN) {
+                notificationService.SendSystemAdminCreatedEmail(account.getEmail());
+            }
+            //update account in database
+            return "";
         }
     }
 
-    public String createAccount(Account account) {
-        //check if the user exists
-        Account userExists = accountRepository.findByEmail(account.getEmail());
-        if (userExists != null) {
-            return "User already exists";
-        }
-        else {
-            accountRepository.save(account);
-            return "Account Created";
-        }
+
+    private String generateVerificationCode() {
+        //generate verification code from 100000 to 999999
+        String code = String.valueOf((int) (Math.random() * (999999 - 100000 + 1) + 100000));
+        //add space between every 3 digits
+        return code;
     }
 
 }
