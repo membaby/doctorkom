5e667b3e6d4f46a97edcc1bfac295f934a9a2bac
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-29 01:55:16 +0200

    Make RegistrationService not throw exceptions

--- backend/src/main/java/com/example/doctorkom/Services/Register_LogIn/RegistrationService.java
+++ backend/src/main/java/com/example/doctorkom/Services/Register_LogIn/RegistrationService.java
@@ -17,7 +17,7 @@ public class RegistrationService {
         this.notificationService = notificationService;
         this.repositoryHandler = repositoryHandler;
     }
-    public String register_Patient(Patient patient) throws MessagingException {
+    public String register_Patient(Patient patient){
         //check if the user exists
         SystemUser systemUser = patient.getSystemUser();
         Account account = systemUser.getAccount();
@@ -34,14 +34,18 @@ public class RegistrationService {
         //generate verification code from 100000 to 999999
         String code = generateVerificationCode();
         //send verification email
-        notificationService.VerificationEmail_PD(account.getEmail(),code);
-        //add account to database
-        Command adder = repositoryHandler.GetCommmand("add");
-        adder.execute(patient);
-        //store verification code and account in database
-            return "";
+        try{
+            notificationService.VerificationEmail_PD(account.getEmail(),code);
+            //add account to database
+            Command adder = repositoryHandler.GetCommmand("add");
+            adder.execute(patient);
+            //store verification code and account in database
+                return "";
+        } catch (MessagingException e) {
+            return "Couldn't send verification email.";
+        }
     }
-    public String register_Doctor(Doctor doctor) throws MessagingException {
+    public String register_Doctor(Doctor doctor){
         SystemUser systemUser = doctor.getSystemUser();
         Account account = systemUser.getAccount();
         Command existenceChecker = repositoryHandler.GetCommmand("check");
@@ -54,12 +58,16 @@ public class RegistrationService {
             return usernameExists;
         }
         String code = generateVerificationCode();
-        notificationService.VerificationEmail_PD(account.getEmail(),code);
-        Command adder = repositoryHandler.GetCommmand("add");
-        adder.execute(doctor);
-        return "";
+        try{
+            notificationService.VerificationEmail_PD(account.getEmail(),code);
+            Command adder = repositoryHandler.GetCommmand("add");
+            adder.execute(doctor);
+            return "";
+        } catch (MessagingException e) {
+            return "Couldn't send verification email.";
+        }
     }
-    public String register_ClinicAdmin(ClinicAdmin clinicAdmin, String formlink) throws MessagingException {
+    public String register_ClinicAdmin(ClinicAdmin clinicAdmin, String formlink) {
         Account account = clinicAdmin.getAccount();
         Command existenceChecker = repositoryHandler.GetCommmand("check");
         String emailExists = existenceChecker.execute(account.getEmail(),"email");
@@ -71,12 +79,17 @@ public class RegistrationService {
             return usernameExists;
         }
         String code = generateVerificationCode();
-        notificationService.VerificationEmail_ClinicAdmin(account.getEmail(),code,formlink);
-        Command adder = repositoryHandler.GetCommmand("add");
-        adder.execute(clinicAdmin);
-        return "";
+        try{
+            notificationService.VerificationEmail_ClinicAdmin(account.getEmail(),code,formlink);
+            Command adder = repositoryHandler.GetCommmand("add");
+            adder.execute(clinicAdmin);
+            return "";
+        } catch (MessagingException e) {
+            return "Couldn't send verification email.";
+        }
+        
     }
-    public String register_SystemAdmin(SystemAdmin systemAdmin,String formlink) throws MessagingException {
+    public String register_SystemAdmin(SystemAdmin systemAdmin,String formlink) {
         Account account = systemAdmin.getAccount();
         Command existenceChecker = repositoryHandler.GetCommmand("check");
         String emailExists = existenceChecker.execute(account.getEmail(),"email");
@@ -88,11 +101,16 @@ public class RegistrationService {
             return usernameExists;
         }
         String code = generateVerificationCode();
-        notificationService.VerificationEmail_SystemAdmin(account.getEmail(),code,formlink);
-        Command adder = repositoryHandler.GetCommmand("add");
-        adder.execute(systemAdmin);
-        //store verification code and account in database
-        return "";
+        
+        try{
+            notificationService.VerificationEmail_SystemAdmin(account.getEmail(),code,formlink);
+            Command adder = repositoryHandler.GetCommmand("add");
+            adder.execute(systemAdmin);
+            //store verification code and account in database
+            return "";
+        } catch (MessagingException e) {
+            return "Couldn't send verification email.";
+        }
     }
 
     public String verify(Account account,String code) throws MessagingException {
