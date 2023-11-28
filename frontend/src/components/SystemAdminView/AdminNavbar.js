3b79761711e262b6745aa537f61a8cacdbdb95ee
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-28 23:55:04 +0200

    SCRUM-49 system admin navbar

--- frontend/src/components/SystemAdminView/AdminNavbar.js
+++ frontend/src/components/SystemAdminView/AdminNavbar.js
@@ -1,5 +1,40 @@
 import React from "react"
+import './styles.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Navbar() {
+
+  var NavbarItems = [
+    { title: 'Home', url: '/system-admin' },
+    { title: 'Add Clinic', url: '/systemAdmin/add-clinic'},
+    { title: 'Remove Clinic', url: '/remove-clinic' },
+    { title: 'Logout', url: '/' },
+  ];
+  
+    return(
+	<>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+
+            <a href="/">
+                <div class="logo">
+                    <img src="/images/logo2.png" alt="logo" />
+                </div>
+            </a>
+                                    
+            <div class="col-5"></div>
 
 
-export default function AdminNavbar(){
-}
+            <div className="col-6 d-flex nav-link text-white riMar  ">
+                {NavbarItems.map((item, index) => (
+                    <div className="nav-item  m-auto" >
+                    <a className="nav-link" href={item.url} style={{ fontSize: '21px' }}>{item.title}</a>
+                    </div>
+                ))}
+            </div> 
+
+        </nav> 
+    </>
+    );
+};
+
