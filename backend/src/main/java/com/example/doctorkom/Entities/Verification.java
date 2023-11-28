df1c2273b5fe6959ea3e0d137e6c801215c1df2d
MostafaGalal1 <mostafag649.mg@gmail.com>
2023-11-28 18:19:12 +0200

    SCRUM-126 added scheduled procedure adding Verification table adding VerificationRepository tests handling some testing issues Not considering integration testing

---
+++ backend/src/main/java/com/example/doctorkom/Entities/Verification.java
@@ -0,0 +1,47 @@
+package com.example.doctorkom.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Verification")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Verification {
+    @Id
+    @Column(name = "AccountId")
+    private Integer id;
+
+    @Column(name = "Code")
+    private String code;
+
+    @Column(name = "CreationTime")
+    private LocalDateTime creationTime;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "AccountId")
+    private Account account;
+
+    public Verification(String code, LocalDateTime creationTime, Account account) {
+        this.code = code;
+        this.creationTime = creationTime;
+        this.account = account;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Verification that = (Verification) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+}
