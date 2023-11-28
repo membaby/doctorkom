4c4ba2503e788e4a53b05e1f5f42cc0e1270cb70
A-Talha <96351797+A-Talha@users.noreply.github.com>
2023-11-29 00:57:30 +0200

    made the repository handler based on factory and command design patterns

--- backend/src/main/java/com/example/doctorkom/Services/Register_LogIn/RegistrationService.java
+++ backend/src/main/java/com/example/doctorkom/Services/Register_LogIn/RegistrationService.java
@@ -2,6 +2,7 @@ package com.example.doctorkom.Services.Register_LogIn;
 
 import com.example.doctorkom.Entities.*;
 import com.example.doctorkom.Services.Notifier.NotificationService;
+import com.example.doctorkom.Services.RepositoryHandler.Commander.Command;
 import com.example.doctorkom.Services.RepositoryHandler.RepositoryHandler;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,93 +21,86 @@ public class RegistrationService {
         //check if the user exists
         SystemUser systemUser = patient.getSystemUser();
         Account account = systemUser.getAccount();
-        boolean emailExists = repositoryHandler.EmailAccountExists(account.getEmail());
-        boolean usernameExists = repositoryHandler.UsernameAccountExists(account.getUsername());
-        if (emailExists) {
-            return "email already exists";
+        Command existenceChecker = repositoryHandler.GetCommmand("check");
+        String emailExists = existenceChecker.execute(account.getEmail(),"email");
+        String usernameExists = existenceChecker.execute(account.getUsername(),"username");
+        //if the emailexists is not empty then the email exists same for username
+        if (!emailExists.isEmpty()) {
+            return emailExists;
         }
-        if (usernameExists) {
-            return "username already exists";
+        if (!usernameExists.isEmpty()) {
+            return usernameExists;
         }
         //generate verification code from 100000 to 999999
         String code = generateVerificationCode();
         //send verification email
         notificationService.VerificationEmail_PD(account.getEmail(),code);
         //add account to database
-        repositoryHandler.AddAccount(account);
-        repositoryHandler.AddSystemUser(systemUser);
-        repositoryHandler.AddPatient(patient);
+        Command adder = repositoryHandler.GetCommmand("add");
+        adder.execute(patient);
         //store verification code and account in database
             return "";
     }
     public String register_Doctor(Doctor doctor) throws MessagingException {
-        //check if the user exists
         SystemUser systemUser = doctor.getSystemUser();
         Account account = systemUser.getAccount();
-        boolean emailExists = repositoryHandler.EmailAccountExists(account.getEmail());
-        boolean usernameExists = repositoryHandler.UsernameAccountExists(account.getUsername());
-        if (emailExists) {
-            return "email already exists";
+        Command existenceChecker = repositoryHandler.GetCommmand("check");
+        String emailExists = existenceChecker.execute(account.getEmail(),"email");
+        String usernameExists = existenceChecker.execute(account.getUsername(),"username");
+        if (!emailExists.isEmpty()) {
+            return emailExists;
         }
-        if (usernameExists) {
-            return "username already exists";
+        if (!usernameExists.isEmpty()) {
+            return usernameExists;
         }
-        //generate verification code from 100000 to 999999
         String code = generateVerificationCode();
-        //send verification email
         notificationService.VerificationEmail_PD(account.getEmail(),code);
-        //add account to database
-        repositoryHandler.AddAccount(account);
-        repositoryHandler.AddSystemUser(systemUser);
-        repositoryHandler.AddDoctor(doctor);
-        //store verification code and account in database
+        Command adder = repositoryHandler.GetCommmand("add");
+        adder.execute(doctor);
         return "";
     }
     public String register_ClinicAdmin(ClinicAdmin clinicAdmin, String formlink) throws MessagingException {
-        //check if the user exists
         Account account = clinicAdmin.getAccount();
-        boolean emailExists = repositoryHandler.EmailAccountExists(account.getEmail());
-        boolean usernameExists = repositoryHandler.UsernameAccountExists(account.getUsername());
-        if (emailExists) {
-            return "email already exists";
+        Command existenceChecker = repositoryHandler.GetCommmand("check");
+        String emailExists = existenceChecker.execute(account.getEmail(),"email");
+        String usernameExists = existenceChecker.execute(account.getUsername(),"username");
+        if (!emailExists.isEmpty()) {
+            return emailExists;
         }
-        if (usernameExists) {
-            return "username already exists";
+        if (!usernameExists.isEmpty()) {
+            return usernameExists;
         }
-        //generate verification code from 100000 to 999999
         String code = generateVerificationCode();
-        //send verification email
         notificationService.VerificationEmail_ClinicAdmin(account.getEmail(),code,formlink);
-        //add account to database
-        repositoryHandler.AddAccount(account);
-        repositoryHandler.AddClinicAdmin(clinicAdmin);
-        //store verification code and account in database
+        Command adder = repositoryHandler.GetCommmand("add");
+        adder.execute(clinicAdmin);
         return "";
     }
     public String register_SystemAdmin(SystemAdmin systemAdmin,String formlink) throws MessagingException {
-        //check if the user exists
         Account account = systemAdmin.getAccount();
-        boolean emailExists = repositoryHandler.EmailAccountExists(account.getEmail());
-        boolean usernameExists = repositoryHandler.UsernameAccountExists(account.getUsername());
-        if (emailExists) {
-            return "email already exists";
+        Command existenceChecker = repositoryHandler.GetCommmand("check");
+        String emailExists = existenceChecker.execute(account.getEmail(),"email");
+        String usernameExists = existenceChecker.execute(account.getUsername(),"username");
+        if (!emailExists.isEmpty()) {
+            return emailExists;
         }
-        if (usernameExists) {
-            return "username already exists";
+        if (!usernameExists.isEmpty()) {
+            return usernameExists;
         }
-        //generate verification code from 100000 to 999999
         String code = generateVerificationCode();
-        //send verification email
         notificationService.VerificationEmail_SystemAdmin(account.getEmail(),code,formlink);
-        //add account to database
-        repositoryHandler.AddAccount(account);
-        repositoryHandler.AddSystemAdmin(systemAdmin);
+        Command adder = repositoryHandler.GetCommmand("add");
+        adder.execute(systemAdmin);
         //store verification code and account in database
         return "";
     }
 
     public String verify(Account account,String code) throws MessagingException {
         //verify the account
+        /*-If verified successfully return "success".
+        -If code was incorrect but email was correct return "wrong code".
+        -If email isn't in the system then return "email not registered".
+        -If account is already verified return "already verified*/
         boolean isValid = true;
         if (!isValid) {
             return "Invalid verification code";
@@ -116,10 +110,11 @@ public class RegistrationService {
                 notificationService.SendPatientCreatedEmail(account.getEmail());
             }
             else if (account.getRole() == Role.DOCTOR) {
-                //get doctor name
-                SystemUser systemUser = repositoryHandler.findSystemUserById(account.getId());
-                String name = systemUser.getFirstName();
-                notificationService.SendDoctorCreatedEmail(account.getEmail(),name);
+                //get the doctor name from database
+                Command finder = repositoryHandler.GetCommmand("find");
+                Doctor doctor = finder.execute(account.getEmail(),"email","doctor").getDoctor();
+                SystemUser systemUser = doctor.getSystemUser();
+                notificationService.SendDoctorCreatedEmail(account.getEmail(),systemUser.getFirstName());
             }
             else if (account.getRole() == Role.CLINIC_ADMIN) {
                 notificationService.SendClinicAdminCreatedEmail(account.getEmail());
--- backend/src/main/java/com/example/doctorkom/Services/RepositoryHandler/RepositoryHandler.java
+++ backend/src/main/java/com/example/doctorkom/Services/RepositoryHandler/RepositoryHandler.java
@@ -1,61 +1,39 @@
 package com.example.doctorkom.Services.RepositoryHandler;
 
-import com.example.doctorkom.Entities.*;
-import com.example.doctorkom.Repositories.*;
+
+import com.example.doctorkom.Services.RepositoryHandler.Commander.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RepositoryHandler {
-    private final AccountRepository accountRepository;
-    private final PatientRepository patientRepository;
-    private final DoctorRepository doctorRepository;
-    private final SystemUserRepository systemUserRepository;
-    private final ClinicAdminRepository clinicAdminRepository;
-    private final SystemAdminRepository systemAdminRepository;
+    private final Adder adder;
+    private final Finder finder;
+    private final ExistenceChecker existenceChecker;
+
 
     @Autowired
-    public RepositoryHandler(AccountRepository accountRepository,
-                             PatientRepository patientRepository,
-                             DoctorRepository doctorRepository,
-                             SystemUserRepository systemUserRepository,
-                             ClinicAdminRepository clinicAdminRepository,
-                             SystemAdminRepository systemAdminRepository) {
-        this.accountRepository = accountRepository;
-        this.patientRepository = patientRepository;
-        this.doctorRepository = doctorRepository;
-        this.systemUserRepository = systemUserRepository;
-        this.clinicAdminRepository = clinicAdminRepository;
-        this.systemAdminRepository = systemAdminRepository;
+    public RepositoryHandler(Adder adder, Finder finder, ExistenceChecker existenceChecker) {
+        this.adder = adder;
+        this.finder = finder;
+        this.existenceChecker = existenceChecker;
     }
     //add account to database
-    public void AddAccount(Account account) {
-        accountRepository.save(account);
-    }
-    //add patient to database
-    public void AddPatient(Patient patient) {
-        patientRepository.save(patient);
-    }
-    //add doctor to database
-    public void AddDoctor(Doctor doctor) {
-        doctorRepository.save(doctor);
-    }
-    //add SystemUser to database
-    public void AddSystemUser(SystemUser systemUser) { systemUserRepository.save(systemUser); }
-    //aad ClinicAdmin to database
-    public void AddClinicAdmin(ClinicAdmin clinicAdmin) { clinicAdminRepository.save(clinicAdmin); }
-    //add SystemAdmin to database
-    public void AddSystemAdmin(SystemAdmin systemAdmin) { systemAdminRepository.save(systemAdmin); }
-    //--------------------------------------------------------------------------------------------------
-    //find system user by id
-    public SystemUser findSystemUserById(int id) {
-        return systemUserRepository.findById(id).orElse(null);
-    }
-    public boolean EmailAccountExists(String email) {
-        return accountRepository.findByEmail(email) != null;
-    }
-    public boolean UsernameAccountExists(String username) {
-        return accountRepository.findByUsername(username) != null;
+    public Command GetCommmand(String command){
+        switch (command) {
+            case "add" -> {
+                return adder;
+            }
+            case "find" -> {
+                return finder;
+            }
+            case "check" -> {
+                return existenceChecker;
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 
 
