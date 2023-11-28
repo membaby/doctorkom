a34649fbff15c32476349ce88dabfc451e19801f
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-28 12:32:29 +0200

    SCRUM-129 SignupController uses RegistrationService

--- backend/src/main/java/com/example/doctorkom/Controllers/SignupController.java
+++ backend/src/main/java/com/example/doctorkom/Controllers/SignupController.java
@@ -8,6 +8,7 @@ import com.example.doctorkom.DTOs.PatientDTO;
 import com.example.doctorkom.DTOMappers.DoctorMapper;
 import com.example.doctorkom.DTOMappers.PatientMapper;
 import com.example.doctorkom.Entities.Patient;
+import com.example.doctorkom.Services.Register_LogIn.RegistrationService;
 import com.example.doctorkom.Entities.Doctor;
 
 @RestController
@@ -19,13 +20,15 @@ public class SignupController {
 
 	@Autowired
 	DoctorMapper doctorMapper;
+	@Autowired
+	RegistrationService registrationService;
 	
 	@PutMapping("/patient")
 	public SignupResponse patientSignup(@RequestBody PatientDTO patientDTO) {
 		//Create Patient entity
 		Patient patient = patientMapper.toEntity(patientDTO);
 		//Call bussiness logic to register patient
-		String msg = new DummyRegistrar().tryRegisterPatient(patient);
+		String msg = registrationService.registerPatient(patient);
 		SignupResponse response = new SignupResponse(msg, msg.isEmpty());
 		return response;
 	}
@@ -36,21 +39,11 @@ public class SignupController {
 		//Create doctor entity.
 		Doctor doctor = doctorMapper.toEntity(doctorDTO);
 		//Call bussiness logic to register patient
-		String msg = new DummyRegistrar().tryRegisterDoctor(doctor);
+		String msg = registrationService.registerDoctor(doctor);
 		SignupResponse response = new SignupResponse(msg, msg.isEmpty());
 		return response;
 	}
 
 }
 
-class DummyRegistrar{
-	public String tryRegisterPatient(Patient patient)
-	{
-		return "";
-	}
-	public String tryRegisterDoctor(Doctor doctor)
-	{
-		return "";
-	}
-}
 
