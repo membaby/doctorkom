276eb7b24bd191fb3945474ef82eddb74fa523c1
Sama Zayed <96790595+samatarekzayed@users.noreply.github.com>
2023-11-28 04:29:35 +0200

    create new clinic , search and remove clinic

--- backend/src/main/java/com/example/doctorkom/DoctorkomApplication.java
+++ backend/src/main/java/com/example/doctorkom/DoctorkomApplication.java
@@ -1,7 +1,17 @@
 package com.example.doctorkom;
 
+import com.example.doctorkom.Entities.*;
+import com.example.doctorkom.Repositories.ClinicRepository;
+import com.example.doctorkom.Services.ClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Date;
+
+import static java.time.ZoneOffset.UTC;
 
 @SpringBootApplication
 public class DoctorkomApplication {
@@ -9,5 +19,21 @@ public class DoctorkomApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorkomApplication.class, args);
 	}
+	private final ClinicService clinicService;
+	@Autowired
+	public DoctorkomApplication(ClinicService clinicService) {
+		this.clinicService = clinicService;
+	}
+@Bean
+@Autowired
+public CommandLineRunner commandLineRunner (ClinicRepository clinicRepository) {
+	return runner -> {
+		Clinic clinic=new Clinic("sz","sm","sm","sm","sm");
+//		clinicRepository.save(clinic);
+//		clinicService.createClinic(clinic);
+		clinicService.removeClinic(clinic);
+		System.out.print("deleted semo");
 
+	};
+}
 }
---
+++ backend/src/main/java/com/example/doctorkom/Entities/Clinic.java
@@ -0,0 +1,47 @@
+package com.example.doctorkom.Entities;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "Clinic")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Clinic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Integer id;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Address")
+    private String address;
+
+
+    @Column(name = "Phone")
+    private String phone;
+
+    @Column(name = "Landline")
+    private String landline;
+
+
+    public Clinic(String email, String name, String address, String phone,String landline) {
+        this.email = email;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.landline=landline;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
