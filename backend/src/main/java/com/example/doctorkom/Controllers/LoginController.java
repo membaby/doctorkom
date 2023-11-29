7bda69544b6498ee2b032c7193d689738ec48821
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-29 15:38:57 +0200

    SCRUM-94 Change login controller to use LogInService

--- backend/src/main/java/com/example/doctorkom/Controllers/LoginController.java
+++ backend/src/main/java/com/example/doctorkom/Controllers/LoginController.java
@@ -1,36 +1,52 @@
 package com.example.doctorkom.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.doctorkom.DTOMappers.*;
 import com.example.doctorkom.DTOs.*;
 import com.example.doctorkom.Entities.*;
+import com.example.doctorkom.Services.Register_LogIn.LogInService;
+import com.example.doctorkom.Services.RepositoryHandler.EntityWrapper;
 
 @RestController
 public class LoginController {
+
+    @Autowired
+    AccountMapper accountMapper;
+    @Autowired
+    PatientMapper patientMapper;
+    @Autowired
+    DoctorMapper doctorMapper;
+    @Autowired
+    SystemAdminMapper systemAdminMapper;
+    @Autowired
+    ClinicAdminMapper clinicAdminMapper;
+    @Autowired
+    LogInService logInService;
     
     @PostMapping("/login")
     public LoginResponse attemptLogin(@RequestBody AccountDTO accountDTO)
     {
-        Account partialAccount = AccountDTOMapper.INSTANCE.toEntity(accountDTO);
-        DummyLoginService loginService = new DummyLoginService();
-        Account fullAccount = loginService.login(partialAccount);
+        
+        Account partialAccount = accountMapper.toEntity(accountDTO);
+        EntityWrapper fullAccount = logInService.login(partialAccount);
         if (fullAccount == null)
             return new LoginResponse(false);
 
         switch (fullAccount.getRole())
         {
-            case PATIENT:
-                PatientDTO patientDTO = PatientDTOMapper.INSTANCE.toDTO(loginService.getPatientAccount(fullAccount));
+            case "PATIENT":
+                PatientDTO patientDTO = patientMapper.toDTO(fullAccount.getPatient());
                 return new LoginResponse(true, Role.PATIENT, patientDTO);
-            case DOCTOR:
-                DoctorDTO doctorDTO = DoctorDTOMapper.INSTANCE.toDTO(loginService.getDoctorAccount(fullAccount));
+            case "DOCTOR":
+                DoctorDTO doctorDTO = doctorMapper.toDTO(fullAccount.getDoctor());
                 return new LoginResponse(true, Role.DOCTOR, doctorDTO);
-            case SYSTEM_ADMIN:
-                SystemAdminDTO systemAdminDTO = SystemAdminDTOMapper.INSTANCE.toDTO(loginService.getSystemAdmin(fullAccount));
+            case "SYSTEM_ADMIN":
+                SystemAdminDTO systemAdminDTO = systemAdminMapper.toDTO(fullAccount.getSystemAdmin());
                 return new LoginResponse(true, Role.SYSTEM_ADMIN, systemAdminDTO);
             default: //CLINIC_ADMIN
-                ClinicAdminDTO clinicAdminDTO = ClinicAdminDTOMapper.INSTANCE.toDTO(loginService.getClinicAdmin(fullAccount));
+                ClinicAdminDTO clinicAdminDTO = clinicAdminMapper.toDTO(fullAccount.getClinicAdmin());
                 return new LoginResponse(true, Role.CLINIC_ADMIN, clinicAdminDTO);
         }
     }
@@ -41,31 +57,6 @@ public class LoginController {
         return new DummyPasswordRecoverer().tryRecoverPassword(email);
     }
 
-}
-
-class DummyLoginService{
-    public Account login(Account account)
-    {
-        return new Account();
-    }
-
-    public Patient getPatientAccount(Account account)
-    {
-        return new Patient();
-    }
-
-    public Doctor getDoctorAccount(Account account)
-    {
-        return new Doctor();
-    }
-
-    public SystemAdmin getSystemAdmin(Account account){
-        return new SystemAdmin(0);
-    }
-
-    public ClinicAdmin getClinicAdmin(Account account){
-        return new ClinicAdmin();
-    }
 }
 
 class DummyPasswordRecoverer{
