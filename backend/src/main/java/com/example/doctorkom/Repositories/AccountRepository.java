e601e6e3178db244280595cf9a994268aa779a3d
MostafaGalal1 <mostafag649.mg@gmail.com>
2023-12-07 19:53:34 +0200

    SCRUM-147 Adding WorksFor table and corresponding DAO, DTO and repo

--- backend/src/main/java/com/example/doctorkom/Repositories/AccountRepository.java
+++ backend/src/main/java/com/example/doctorkom/Repositories/AccountRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     Optional<Account> findByEmail(String email);
