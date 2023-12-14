1bad04026f283c6e9de923e07fe5d6bf626cc9ac
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-14 10:13:05 +0200

    SCRUM-166 Change clinic tests to use h2 database and still work

--- backend/src/test/java/com/example/doctorkom/Services/ClinicServiceTest.java
+++ backend/src/test/java/com/example/doctorkom/Services/ClinicServiceTest.java
@@ -3,7 +3,9 @@ package com.example.doctorkom.Services;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.doctorkom.Entities.*;
 import com.example.doctorkom.Repositories.ClinicRepository;
@@ -12,12 +14,14 @@ import com.example.doctorkom.Services.ClinicServices.ClinicService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Date;
 import java.util.List;
 
+@SpringBootTest
 @ActiveProfiles("test")
-@DataJpaTest
 public class ClinicServiceTest {
 
     @Autowired
@@ -27,82 +31,91 @@ public class ClinicServiceTest {
     @Autowired
     ClinicRepository clinicRepo;
     
-    
-    // @Test
-    // void addValidDoctorToClinic() //Adds a valid doctor account to a clinic when it doesn't already work for this clinic
-    // {
-    //     Doctor doctor = buildTestDoctor();
-    //     Clinic clinic = buildTestClinic();
-    //     doctorRepo.save(doctor);
-    //     clinicRepo.save(clinic);
-    //     String str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
-    //     assertEquals("Doctor added successfully", str);
-        
-    //     //Check that this doctor-clinic pair exist in the Works_For table.
-    //     List<Doctor> doctors = clinic.getDoctors();
-    //     List<Clinic> clinics = doctor.getClinics();
-    //     assertTrue(doctors.contains(doctor));
-    //     assertTrue(clinics.contains(clinic));
-    // }
-    
-    // @Test
-    // void addValidPreAddedDoctorToClinic() //Adds a valid doctor account to a clinic when it already works for the clinic
-    // {
-    //     Doctor doctor = buildTestDoctor();
-    //     Clinic clinic = buildTestClinic();
-    //     doctorRepo.save(doctor);
-    //     clinicRepo.save(clinic);
-    //     String str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
-    //     assertEquals("Doctor added successfully", str);
-    //     str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
-    //     assertEquals("Doctor already in the clinic", str);
 
-    //     //Check that this doctor-clinic pair exist in the Works_For table.
-    //     List<Doctor> doctors = clinic.getDoctors();
-    //     List<Clinic> clinics = doctor.getClinics();
-    //     assertTrue(doctors.contains(doctor));
-    //     assertTrue(clinics.contains(clinic));
-    // }
+    @Test
+    void testingH2()
+    {
+        Clinic clinic = buildTestClinic();
+        clinicRepo.save(clinic);
+        Clinic existingClinic = clinicRepo.findById(clinic.getId()).orElse(null);
+        assertNotNull(existingClinic);
+    }
     
-    // @Test
-    // void addInvalidDoctorToClinic() //Adds an invalid doctor account to a clinic
-    // {
-    //     Doctor doctor = buildTestDoctor();
-    //     Clinic clinic = buildTestClinic();
-    //     doctorRepo.save(doctor);
-    //     clinicRepo.save(clinic);
-    //     String str = clinicService.AddDoctorToClinic("a@b.c", clinic);
-    //     assertEquals("Doctor not found", str); 
-    //     //Check the clinic has no doctors
-    //     List<Doctor> doctors = clinic.getDoctors();
-    //     assertTrue(doctors.isEmpty());
-    // }
+    @Test
+    void addValidDoctorToClinic() //Adds a valid doctor account to a clinic when it doesn't already work for this clinic
+    {
+        Doctor doctor = buildTestDoctor();
+        Clinic clinic = buildTestClinic();
+        doctorRepo.save(doctor);
+        clinicRepo.save(clinic);
+        String str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
+        assertEquals("Doctor added successfully", str);
+        
+        //Check that this doctor-clinic pair exist in the Works_For table.
+        List<Doctor> doctors = clinic.getDoctors();
+        List<Clinic> clinics = doctor.getClinics();
+        assertTrue(doctors.contains(doctor));
+        assertTrue(clinics.contains(clinic));
+    }
     
-    // @Test
-    // void addValidDoctorToClinic2() //Adds a valid doctor account to 2 different clinics
-    // {
-    //     Doctor doctor = buildTestDoctor();
-    //     Clinic clinic = buildTestClinic();
-    //     Clinic clinic2 = buildTestClinic();
-    //     Account c2Acc = clinic2.getAdmin().getAccount();
-    //     c2Acc.setEmail("clinic@g-mail.com");
-    //     c2Acc.setUsername("null");
-    //     doctorRepo.save(doctor);
-    //     clinicRepo.save(clinic);
-    //     clinicRepo.save(clinic2);
-    //     String str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
-    //     assertEquals("Doctor added successfully", str); 
-    //     str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic2);
-    //     assertEquals("Doctor added successfully", str);
-    //     //Check the doctor exists in each clinic and both clinics exist in the doctor
-    //     List<Doctor> doctors1 = clinic.getDoctors();
-    //     List<Doctor> doctors2 = clinic2.getDoctors();
-    //     List<Clinic> clinics = doctor.getClinics(); 
-    //     assertTrue(doctors1.contains(doctor));
-    //     assertTrue(doctors2.contains(doctor));
-    //     assertTrue(clinics.contains(clinic));
-    //     assertTrue(clinics.contains(clinic2));
-    // }
+    @Test
+    void addValidPreAddedDoctorToClinic() //Adds a valid doctor account to a clinic when it already works for the clinic
+    {
+        Doctor doctor = buildTestDoctor();
+        Clinic clinic = buildTestClinic();
+        doctorRepo.save(doctor);
+        clinicRepo.save(clinic);
+        String str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
+        assertEquals("Doctor added successfully", str);
+        str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
+        assertEquals("Doctor already in the clinic", str);
+
+        //Check that this doctor-clinic pair exist in the Works_For table.
+        List<Doctor> doctors = clinic.getDoctors();
+        List<Clinic> clinics = doctor.getClinics();
+        assertTrue(doctors.contains(doctor));
+        assertTrue(clinics.contains(clinic));
+    }
+    
+    @Test
+    void addInvalidDoctorToClinic() //Adds an invalid doctor account to a clinic
+    {
+        Doctor doctor = buildTestDoctor();
+        Clinic clinic = buildTestClinic();
+        doctorRepo.save(doctor);
+        clinicRepo.save(clinic);
+        String str = clinicService.AddDoctorToClinic("a@b.c", clinic);
+        assertEquals("Doctor not found", str); 
+        //Check the clinic has no doctors
+        List<Doctor> doctors = clinic.getDoctors();
+        assertTrue(doctors.isEmpty());
+    }
+    
+    @Test
+    void addValidDoctorToClinic2() //Adds a valid doctor account to 2 different clinics
+    {
+        Doctor doctor = buildTestDoctor();
+        Clinic clinic = buildTestClinic();
+        Clinic clinic2 = buildTestClinic();
+        Account c2Acc = clinic2.getAdmin().getAccount();
+        c2Acc.setEmail("clinic@g-mail.com");
+        c2Acc.setUsername("null");
+        doctorRepo.save(doctor);
+        clinicRepo.save(clinic);
+        clinicRepo.save(clinic2);
+        String str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic);
+        assertEquals("Doctor added successfully", str); 
+        str = clinicService.AddDoctorToClinic(doctor.getSystemUser().getAccount().getEmail(), clinic2);
+        assertEquals("Doctor added successfully", str);
+        //Check the doctor exists in each clinic and both clinics exist in the doctor
+        List<Doctor> doctors1 = clinic.getDoctors();
+        List<Doctor> doctors2 = clinic2.getDoctors();
+        List<Clinic> clinics = doctor.getClinics(); 
+        assertTrue(doctors1.contains(doctor));
+        assertTrue(doctors2.contains(doctor));
+        assertTrue(clinics.contains(clinic));
+        assertTrue(clinics.contains(clinic2));
+    }
 
     @Test
     void removeExistingDoctorFromClinic() //Removes a doctor from a clinic when it already works for this clinic
@@ -215,6 +228,7 @@ public class ClinicServiceTest {
         mobilePhone("123456789").
         landlinePhone("123456789").
         address("asdfasdf").
+        birthdate(Date.valueOf("1999-01-01")).
         gender(Gender.FEMALE).build();
         Doctor doctor = Doctor.builder().systemUser(user).
         specialty(DoctorSpecialty.ANESTHESIOLOGIST).
