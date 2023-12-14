cbe08110f8bc2f4ac798a46ad65b42880ac08458
A-Talha <96351797+A-Talha@users.noreply.github.com>
2023-12-14 15:08:17 +0200

    added some controllers tests

--- backend/src/test/java/com/example/doctorkom/Controllers/ClinicControllerTest.java
+++ backend/src/test/java/com/example/doctorkom/Controllers/ClinicControllerTest.java
@@ -2,16 +2,20 @@ package com.example.doctorkom.Controllers;
 
 import com.example.doctorkom.Controllers.ClinicController.ClinicController;
 import com.example.doctorkom.DTOMappers.ClinicMapper;
-import com.example.doctorkom.DTOs.AccountDTO;
-import com.example.doctorkom.DTOs.ClinicAdminDTO;
-import com.example.doctorkom.DTOs.ClinicDTO;
-import com.example.doctorkom.DTOs.ClinicDoctorDTO;
+import com.example.doctorkom.DTOMappers.DoctorMapper;
+import com.example.doctorkom.DTOMappers.TimeSlotMapper;
+import com.example.doctorkom.DTOs.*;
 import com.example.doctorkom.Entities.Clinic;
+import com.example.doctorkom.Entities.Doctor;
+import com.example.doctorkom.Entities.TimeSlot;
 import com.example.doctorkom.Services.ClinicServices.ClinicService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -24,9 +28,83 @@ public class ClinicControllerTest {
 
     @Mock
     private ClinicService clinicService;
+
+    @Mock
+    private TimeSlotMapper timeSlotMapper;
+    @Mock
+    private DoctorMapper doctorMapper;
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void editClinicInfo(){
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+
+        when(clinicMapper.toEntity(clinicDTOTest)).thenReturn(clinicTest);
+        when(clinicService.editClinicInfo(clinicTest)).thenReturn("Clinic edited successfully");
+
+        when(clinicController.EditClinicInfo(clinicDTOTest)).thenAnswer(invocation -> {
+            ClinicDTO arg = invocation.getArgument(0);
+            Clinic clinic = clinicMapper.toEntity(arg);
+            return clinicService.editClinicInfo(clinic);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.EditClinicInfo(clinicDTOTest);
+
+        // Verify the result
+        assertEquals("Clinic edited successfully", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).EditClinicInfo(clinicDTOTest);
+        verify(clinicService, times(1)).editClinicInfo(clinicTest);
+        verify(clinicMapper, times(1)).toEntity(clinicDTOTest);
+    }
+
+    @Test
+    void getDoctors(){
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+        DoctorDTO doctorDTOTest = DoctorDTO.builder().build();
+        List<Doctor> doctors = new ArrayList<>();
+        doctors.add(Doctor.builder().build());
+        doctors.add(Doctor.builder().build());
+        doctors.add(Doctor.builder().build());
+        List<DoctorDTO> doctorDTOSr = new ArrayList<>();
+        for (Doctor doctor : doctors) {
+            doctorDTOSr.add(doctorDTOTest);
+        }
+
+
+        when(clinicMapper.toEntity(clinicDTOTest)).thenReturn(clinicTest);
+        when(clinicService.GetDoctorsForClinic(clinicTest)).thenReturn(doctors);
+        when(doctorMapper.toDto(any())).thenReturn(doctorDTOTest);
+
+        when(clinicController.Doctors(clinicDTOTest)).thenAnswer(invocation -> {
+            ClinicDTO arg = invocation.getArgument(0);
+            Clinic clinic = clinicMapper.toEntity(arg);
+            List<Doctor> doctorList = clinicService.GetDoctorsForClinic(clinic);
+            List<DoctorDTO> doctorDTOS = new ArrayList<>();
+            for (Doctor doctor : doctorList) {
+                doctorDTOS.add(doctorMapper.toDto(doctor));
+            }
+            return doctorDTOS;
+        });
+
+        // Invoke the actual method being tested
+        List<DoctorDTO> response = clinicController.Doctors(clinicDTOTest);
+
+        // Verify the result
+        assertEquals(doctorDTOSr, response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).Doctors(clinicDTOTest);
+        verify(clinicService, times(1)).GetDoctorsForClinic(clinicTest);
+        verify(clinicMapper, times(1)).toEntity(clinicDTOTest);
+        verify(doctorMapper, times(doctors.size())).toDto(any());
     }
 
     @Test
@@ -202,6 +280,206 @@ public class ClinicControllerTest {
         verify(clinicController, times(1)).RemoveDoctor(clinicDoctorDTO);
         verify(clinicService, times(1)).RemoveDoctorFromClinic(doctorEmail, clinicTest);
         verify(clinicMapper, times(1)).toEntity(clinicDTOTest);
+    }
+
+    @Test
+    void EditClinicInfo_ClinicExist() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+
+        when(clinicMapper.toEntity(clinicDTOTest)).thenReturn(clinicTest);
+        when(clinicService.editClinicInfo(clinicTest)).thenReturn("Clinic edited successfully");
+
+        when(clinicController.EditClinicInfo(clinicDTOTest)).thenAnswer(invocation -> {
+            ClinicDTO arg = invocation.getArgument(0);
+            Clinic clinic = clinicMapper.toEntity(arg);
+            return clinicService.editClinicInfo(clinic);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.EditClinicInfo(clinicDTOTest);
+
+        // Verify the result
+        assertEquals("Clinic edited successfully", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).EditClinicInfo(clinicDTOTest);
+        verify(clinicService, times(1)).editClinicInfo(clinicTest);
+        verify(clinicMapper, times(1)).toEntity(clinicDTOTest);
+    }
+
+    @Test
+    void EditClinicInfo_ClinicNotExist() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+
+        when(clinicMapper.toEntity(clinicDTOTest)).thenReturn(clinicTest);
+        when(clinicService.editClinicInfo(clinicTest)).thenReturn("Clinic not found");
+
+        when(clinicController.EditClinicInfo(clinicDTOTest)).thenAnswer(invocation -> {
+            ClinicDTO arg = invocation.getArgument(0);
+            Clinic clinic = clinicMapper.toEntity(arg);
+            return clinicService.editClinicInfo(clinic);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.EditClinicInfo(clinicDTOTest);
+
+        // Verify the result
+        assertEquals("Clinic not found", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).EditClinicInfo(clinicDTOTest);
+        verify(clinicService, times(1)).editClinicInfo(clinicTest);
+        verify(clinicMapper, times(1)).toEntity(clinicDTOTest);
+    }
+
+
+    @Test
+    void AddTimeSlot_TimeSlotAvailable() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        TimeSlotDTO timeSlotDTOTest = TimeSlotDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+        TimeSlot timeSlotTest = TimeSlot.builder().build();
+
+        when(timeSlotMapper.toEntity(timeSlotDTOTest)).thenReturn(timeSlotTest);
+        when(clinicService.AddTimeSlot(timeSlotTest)).thenReturn("Time slot added successfully");
+
+        when(clinicController.AddTimeSlot(timeSlotDTOTest)).thenAnswer(invocation -> {
+            TimeSlotDTO arg = invocation.getArgument(0);
+            TimeSlot timeSlot = timeSlotMapper.toEntity(arg);
+            return clinicService.AddTimeSlot(timeSlot);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.AddTimeSlot(timeSlotDTOTest);
+
+        // Verify the result
+        assertEquals("Time slot added successfully", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).AddTimeSlot(timeSlotDTOTest);
+        verify(clinicService, times(1)).AddTimeSlot(timeSlotTest);
+        verify(timeSlotMapper, times(1)).toEntity(timeSlotDTOTest);
+    }
+    
+    @Test
+    void AddTimeSlot_TimeSlotCollidesWithDoctor() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        TimeSlotDTO timeSlotDTOTest = TimeSlotDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+        TimeSlot timeSlotTest = TimeSlot.builder().build();
+
+        when(timeSlotMapper.toEntity(timeSlotDTOTest)).thenReturn(timeSlotTest);
+        when(clinicService.AddTimeSlot(timeSlotTest)).thenReturn("Time slot Collides with doctor");
+
+        when(clinicController.AddTimeSlot(timeSlotDTOTest)).thenAnswer(invocation -> {
+            TimeSlotDTO arg = invocation.getArgument(0);
+            TimeSlot timeSlot = timeSlotMapper.toEntity(arg);
+            return clinicService.AddTimeSlot(timeSlot);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.AddTimeSlot(timeSlotDTOTest);
+
+        // Verify the result
+        assertEquals("Time slot Collides with doctor", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).AddTimeSlot(timeSlotDTOTest);
+        verify(clinicService, times(1)).AddTimeSlot(timeSlotTest);
+        verify(timeSlotMapper, times(1)).toEntity(timeSlotDTOTest);
+    }
+
+    @Test
+    void AddTimeSlot_TimeSlotCollidesWithAnotherTimeSlot() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        TimeSlotDTO timeSlotDTOTest = TimeSlotDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+        TimeSlot timeSlotTest = TimeSlot.builder().build();
+
+        when(timeSlotMapper.toEntity(timeSlotDTOTest)).thenReturn(timeSlotTest);
+        when(clinicService.AddTimeSlot(timeSlotTest)).thenReturn("Time slot Collides with another time slot");
+
+        when(clinicController.AddTimeSlot(timeSlotDTOTest)).thenAnswer(invocation -> {
+            TimeSlotDTO arg = invocation.getArgument(0);
+            TimeSlot timeSlot = timeSlotMapper.toEntity(arg);
+            return clinicService.AddTimeSlot(timeSlot);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.AddTimeSlot(timeSlotDTOTest);
+
+        // Verify the result
+        assertEquals("Time slot Collides with another time slot", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).AddTimeSlot(timeSlotDTOTest);
+        verify(clinicService, times(1)).AddTimeSlot(timeSlotTest);
+        verify(timeSlotMapper, times(1)).toEntity(timeSlotDTOTest);
+    }
+
+    @Test
+    void RemoveTimeSlot_TimeSlotExist() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        TimeSlotDTO timeSlotDTOTest = TimeSlotDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+        TimeSlot timeSlotTest = TimeSlot.builder().build();
+
+        when(timeSlotMapper.toEntity(timeSlotDTOTest)).thenReturn(timeSlotTest);
+        when(clinicService.RemoveTimeSlot(timeSlotTest)).thenReturn("Time slot removed successfully");
+
+        when(clinicController.RemoveTimeSlot(timeSlotDTOTest)).thenAnswer(invocation -> {
+            TimeSlotDTO arg = invocation.getArgument(0);
+            TimeSlot timeSlot = timeSlotMapper.toEntity(arg);
+            return clinicService.RemoveTimeSlot(timeSlot);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.RemoveTimeSlot(timeSlotDTOTest);
+
+        // Verify the result
+        assertEquals("Time slot removed successfully", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).RemoveTimeSlot(timeSlotDTOTest);
+        verify(clinicService, times(1)).RemoveTimeSlot(timeSlotTest);
+        verify(timeSlotMapper, times(1)).toEntity(timeSlotDTOTest);
+    }
+
+    @Test
+    void RemoveTimeSlot_TimeSlotNotExist() {
+        ClinicDTO clinicDTOTest = ClinicDTOBuilder();
+        TimeSlotDTO timeSlotDTOTest = TimeSlotDTOBuilder();
+        Clinic clinicTest = Clinic.builder().build();
+        TimeSlot timeSlotTest = TimeSlot.builder().build();
+
+        when(timeSlotMapper.toEntity(timeSlotDTOTest)).thenReturn(timeSlotTest);
+        when(clinicService.RemoveTimeSlot(timeSlotTest)).thenReturn("Time slot not found");
+
+        when(clinicController.RemoveTimeSlot(timeSlotDTOTest)).thenAnswer(invocation -> {
+            TimeSlotDTO arg = invocation.getArgument(0);
+            TimeSlot timeSlot = timeSlotMapper.toEntity(arg);
+            return clinicService.RemoveTimeSlot(timeSlot);
+        });
+
+        // Invoke the actual method being tested
+        String response = clinicController.RemoveTimeSlot(timeSlotDTOTest);
+
+        // Verify the result
+        assertEquals("Time slot not found", response);
+
+        // Verify the calls
+        verify(clinicController, times(1)).RemoveTimeSlot(timeSlotDTOTest);
+        verify(clinicService, times(1)).RemoveTimeSlot(timeSlotTest);
+        verify(timeSlotMapper, times(1)).toEntity(timeSlotDTOTest);
+    }
+
+
+
+    private TimeSlotDTO TimeSlotDTOBuilder() {
+        TimeSlotDTO timeSlotDTO = TimeSlotDTO.builder().build();
+        return timeSlotDTO;
     }
 
     private ClinicDTO ClinicDTOBuilder() {
