8918386e8d087da94387cf1d7b504d764adf3657
Mahmoud Embaby <mah.embaby@outlook.com>
2023-12-27 06:22:25 +0200

    UI/UX Refactoring + About Page + Footer

--- frontend/src/components/register/index.js
+++ frontend/src/components/register/index.js
@@ -143,248 +143,239 @@ const Register = ({ userType }) => {
   };
 
   return (
-    <div className="card mx-auto mt-5" style={{ maxWidth: "1000px" }}>
-      <div className="card-body">
-        <h2 className="card-title text-center mb-4">
-          {userType === "patient"
-            ? "Patient Registration"
-            : userType === "doctor"
-            ? "Doctor Registration"
-            : userType === "admin"
-            ? "Admin Registration"
-            : userType === "clinic"
-            ? "Clinic Registration"
-            : ""}
-        </h2>
+    <div className="grayBackground">
+      <div className="container vh-100 d-flex align-items-center justify-content-center">
+        <div className="panelCard" style={{ width: "1000px" }}>
+          <div className="card-body">
+            <h2 className="card-title text-center mb-4">
+              {userType === "patient"
+                ? "Patient Registration"
+                : userType === "doctor"
+                ? "Doctor Registration"
+                : userType === "admin"
+                ? "Admin Registration"
+                : userType === "clinic"
+                ? "Clinic Registration"
+                : ""}
+            </h2>
 
-        <div
-          className="alert alert-primary"
-          role="alert"
-          id="display"
-          style={{ display: "none" }}
-        ></div>
+            <div
+              className="alert alert-primary"
+              role="alert"
+              id="display"
+              style={{ display: "none" }}
+            ></div>
 
-        <form onSubmit={(e) => e.preventDefault()}>
-          {/* Fields */}
-          <div className="row">
-            <div className="col mb-3">
-              <label className="form-label">First Name</label>
-              <input
-                type="text"
-                className="form-control"
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </div>
-
-            <div className="col mb-3">
-              <label className="form-label">Last Name</label>
-              <input
-                type="text"
-                className="form-control"
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
-
-            <div className="col mb-3">
-              <label className="form-label">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col mb-3">
-              <label className="form-label">Username</label>
-              <input
-                type="text"
-                className="form-control"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-
-            <div className="col mb-3">
-              <label className="form-label">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-
-            <div className="col mb-3">
-              <label className="form-label">Gender</label>
-              <select
-                className="form-select"
-                onChange={(e) => setGender(e.target.value)}
-              >
-                <option value="">Select</option>
-                <option value="MALE">Male</option>
-                <option value="FEMALE">Female</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col mb-3">
-              <label className="form-label">Brithdate</label>
-              <input
-                type="date"
-                className="form-control"
-                onChange={(e) => setBirthdate(e.target.value)}
-              />
-            </div>
-
-            <div className="col mb-3">
-              <label className="form-label">Address</label>
-              <input
-                type="text"
-                className="form-control"
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </div>
-
-            <div className="col mb-3">
-              <label className="form-label">Landline</label>
-              <input
-                type="text"
-                className="form-control"
-                onChange={(e) => setLandline(e.target.value)}
-              />
-            </div>
-          </div>
-
-          {userType === "patient" ? (
-            <>
+            <form onSubmit={(e) => e.preventDefault()}>
+              {/* Fields */}
               <div className="row">
                 <div className="col mb-3">
-                  <label className="form-label">Phone</label>
+                  <label className="form-label">First Name</label>
                   <input
                     type="text"
                     className="form-control"
-                    onChange={(e) => setPhone(e.target.value)}
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </div>
 
                 <div className="col mb-3">
-                  <label className="form-label">Marital Status</label>
-                  <select
-                    className="form-select"
-                    onChange={(e) => setMaritalstatus(e.target.value)}
-                  >
-                    <option value="">Select</option>
-                    <option value="single">Single</option>
-                    <option value="married">Married</option>
-                    <option value="divorced">Divorced</option>
-                    <option value="widowed">widowed</option>
-                  </select>
-                </div>
-
-                <div className="col mb-3">
-                  <label className="form-label">Occupation</label>
+                  <label className="form-label">Last Name</label>
                   <input
                     type="text"
                     className="form-control"
-                    onChange={(e) => setOccupation(e.target.value)}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+
+                <div className="col mb-3">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
               </div>
 
               <div className="row">
                 <div className="col mb-3">
-                  <label className="form-label">Insurance</label>
+                  <label className="form-label">Username</label>
                   <input
                     type="text"
                     className="form-control"
-                    onChange={(e) => setInsurance(e.target.value)}
+                    onChange={(e) => setUsername(e.target.value)}
                   />
                 </div>
+
+                <div className="col mb-3">
+                  <label className="form-label">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="col mb-3">
+                  <label className="form-label">Gender</label>
+                  <select
+                    className="form-select"
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="">Select</option>
+                    <option value="MALE">Male</option>
+                    <option value="FEMALE">Female</option>
+                  </select>
+                </div>
               </div>
-            </>
-          ) : userType === "doctor" ? (
-            <>
+
               <div className="row">
                 <div className="col mb-3">
-                  <label className="form-label">Phone</label>
+                  <label className="form-label">Brithdate</label>
                   <input
-                    type="text"
+                    type="date"
                     className="form-control"
-                    onChange={(e) => setPhone(e.target.value)}
+                    onChange={(e) => setBirthdate(e.target.value)}
                   />
                 </div>
 
                 <div className="col mb-3">
-                  <label className="form-label">Title</label>
-                  <select
-                    className="form-select"
-                    onChange={(e) => setTitle(e.target.value)}
-                  >
-                    <option value="">Select</option>
-                    <option value="PROFESSOR">PROFESSOR</option>
-                    <option value="LECTURER">LECTURER</option>
-                    <option value="CONSULTANT">CONSULTANT</option>
-                    <option value="SPECIALIST">SPECIALIST</option>
-                  </select>
+                  <label className="form-label">Address</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
                 </div>
 
                 <div className="col mb-3">
-                  <label className="form-label">Specialty</label>
-                  <select
-                    className="form-select"
-                    onChange={(e) => setSpecialty(e.target.value)}
-                  >
-                    <option value="">Select</option>
-                    <option value="GENERAL_PRACTITIONER">
-                      GENERAL_PRACTITIONER
-                    </option>
-                    <option value="CARDIOLOGIST">CARDIOLOGIST</option>
-                    <option value="DERMATOLOGIST">DERMATOLOGIST</option>
-                    <option value="PEDIATRICIAN">PEDIATRICIAN</option>
-                    <option value="ORTHOPEDIC_SURGEON">
-                      ORTHOPEDIC_SURGEON
-                    </option>
-                    <option value="GYNECOLOGIST">GYNECOLOGIST</option>
-                    <option value="OPHTHALMOLOGIST">OPHTHALMOLOGIST</option>
-                    <option value="NEUROLOGIST">NEUROLOGIST</option>
-                    <option value="UROLOGIST">UROLOGIST</option>
-                    <option value="ENT_SPECIALIST">ENT_SPECIALIST</option>
-                    <option value="PSYCHIATRIST">PSYCHIATRIST</option>
-                    <option value="ONCOLOGIST">ONCOLOGIST</option>
-                    <option value="RADIOLOGIST">RADIOLOGIST</option>
-                    <option value="ANESTHESIOLOGIST">ANESTHESIOLOGIST</option>
-                    <option value="DENTAL_SURGEON">DENTAL_SURGEON</option>
-                  </select>
+                  <label className="form-label">Landline</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setLandline(e.target.value)}
+                  />
                 </div>
               </div>
-            </>
-          ) : (
-            <></>
-          )}
 
-          {/* Buttons */}
-          <div className="row mt-3">
-            <div className="col mb-3">
-              <button
-                className="btn btn-success w-100"
-                onClick={handleRegister}
-              >
-                Register
-              </button>
-            </div>
-            <div className="col mb-3">
-              <button
-                className="btn btn-danger w-100"
-                onClick={() => {
-                  googleRegister();
-                }}
-              >
-                Sign up with Google
-              </button>
-            </div>
+              {userType === "patient" ? (
+                <>
+                  <div className="row">
+                    <div className="col mb-3">
+                      <label className="form-label">Phone</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="col mb-3">
+                      <label className="form-label">Marital Status</label>
+                      <select
+                        className="form-select"
+                        onChange={(e) => setMaritalstatus(e.target.value)}
+                      >
+                        <option value="">Select</option>
+                        <option value="single">Single</option>
+                        <option value="married">Married</option>
+                        <option value="divorced">Divorced</option>
+                        <option value="widowed">widowed</option>
+                      </select>
+                    </div>
+
+                    <div className="col mb-3">
+                      <label className="form-label">Occupation</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        onChange={(e) => setOccupation(e.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col mb-3">
+                      <label className="form-label">Insurance</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        onChange={(e) => setInsurance(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </>
+              ) : userType === "doctor" ? (
+                <>
+                  <div className="row">
+                    <div className="col mb-3">
+                      <label className="form-label">Phone</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </div>
+
+                    <div className="col mb-3">
+                      <label className="form-label">Title</label>
+                      <select
+                        className="form-select"
+                        onChange={(e) => setTitle(e.target.value)}
+                      >
+                        <option value="">Select</option>
+                        <option value="PROFESSOR">PROFESSOR</option>
+                        <option value="LECTURER">LECTURER</option>
+                        <option value="CONSULTANT">CONSULTANT</option>
+                        <option value="SPECIALIST">SPECIALIST</option>
+                      </select>
+                    </div>
+
+                    <div className="col mb-3">
+                      <label className="form-label">Specialty</label>
+                      <select
+                        className="form-select"
+                        onChange={(e) => setSpecialty(e.target.value)}
+                      >
+                        <option value="">Select</option>
+                        <option value="GENERAL_PRACTITIONER">
+                          GENERAL_PRACTITIONER
+                        </option>
+                        <option value="CARDIOLOGIST">CARDIOLOGIST</option>
+                        <option value="DERMATOLOGIST">DERMATOLOGIST</option>
+                        <option value="PEDIATRICIAN">PEDIATRICIAN</option>
+                        <option value="ORTHOPEDIC_SURGEON">
+                          ORTHOPEDIC_SURGEON
+                        </option>
+                        <option value="GYNECOLOGIST">GYNECOLOGIST</option>
+                        <option value="OPHTHALMOLOGIST">OPHTHALMOLOGIST</option>
+                        <option value="NEUROLOGIST">NEUROLOGIST</option>
+                        <option value="UROLOGIST">UROLOGIST</option>
+                        <option value="ENT_SPECIALIST">ENT_SPECIALIST</option>
+                        <option value="PSYCHIATRIST">PSYCHIATRIST</option>
+                        <option value="ONCOLOGIST">ONCOLOGIST</option>
+                        <option value="RADIOLOGIST">RADIOLOGIST</option>
+                        <option value="ANESTHESIOLOGIST">ANESTHESIOLOGIST</option>
+                        <option value="DENTAL_SURGEON">DENTAL_SURGEON</option>
+                      </select>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+
+              {/* Buttons */}
+              <div className="row mt-3">
+                <div className="d-grid d-flex justify-content-center">
+                  <button className="custom-button custom-button-primary w-100 mx-1" onClick={handleRegister}>Register</button>
+                    <div className="d-flex align-items-center justify-content-center mx-2">OR</div>
+                  <button className="custom-button custom-button-tertiary w-100 mx-1" onClick={()=>{googleRegister()}} >Sign up with Google</button>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
