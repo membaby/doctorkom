7f4df0ce19ecede5aefeb5bb00b28029dc67ff4c
A-Talha <96351797+A-Talha@users.noreply.github.com>
2023-12-28 00:49:02 +0200

    done Doctor Dashboard
    added a way to add medical note in the appointment

--- frontend/src/components/DoctorView/DashBoardComponents/DashBoardElements/Elements/Appointments/Appointement/AppointmentListDiv.js
+++ frontend/src/components/DoctorView/DashBoardComponents/DashBoardElements/Elements/Appointments/Appointement/AppointmentListDiv.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // import the styles
+import MedicalNote from './../../MedicalNotes/MedicalNote/MedicalNoteClass.js'
+import PostMedicalNote from './PostMedicalNote.js'
+import RemoveAppointment from './RemoveAppointment.js'
 
 export default function AppointmentListDiv({ appointment }) {
     const { timeSlot, patient } = appointment;
@@ -7,7 +12,12 @@ export default function AppointmentListDiv({ appointment }) {
     const { clinic, date, startTime, endTime } = timeSlot;
     const { firstName, lastName } = patient.systemUser;
 
+    const id = patient.id+date+startTime;
+
     const [isFormVisible, setFormVisibility] = useState(false);
+    const [diagnosis, setDiagnosis] = useState('');
+    const [prescription, setPrescription] = useState('');
+    const [investigations, setInvestigations] = useState('');
 
     // Calculate day and duration
     const day = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date(date));
@@ -19,8 +29,29 @@ export default function AppointmentListDiv({ appointment }) {
         setFormVisibility((prevVisibility) => !prevVisibility);
     };
 
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        let newinvestigations = `${investigations}`;
+        let newprescription = `${prescription}`;
+        let newdiagnosis = `${diagnosis}`;
+        const medicalNote = new MedicalNote(date, newdiagnosis, newinvestigations, newprescription, patient, timeSlot.doctor);
+        if (newinvestigations !== '' || newprescription !== '' || newdiagnosis !== '') {
+            PostMedicalNote(medicalNote);
+            console.log(medicalNote);
+        }
+        RemoveAppointment(appointment);
+        console.log(appointment);
+        //delete the current appointment div with id = id
+        const appointmentDiv = document.getElementById(id);
+        if (appointmentDiv) {
+            appointmentDiv.remove();
+        }
+
+
+    };
+
     return (
-        <div
+        <div id = {id}
             className="list-group-item list-group-item-action d-flex gap-3 py-3"
             style={{
                 border: '1px solid rgb(36, 44, 60)',
@@ -55,24 +86,23 @@ export default function AppointmentListDiv({ appointment }) {
                 {isFormVisible ? 'Hide Form' : 'Show Form'}
                 </button>
                 {isFormVisible && (
-                     <form>
-                        <div class="mb-3">
-                            <label for="diagnosis" class="form-label">diagnosis</label>
-                            <input  type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                     <form onSubmit={handleFormSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="diagnosis" className="form-label"><h5>Diagnosis</h5></label>
+                            <ReactQuill value={diagnosis} onChange={(value) => setDiagnosis(value)} />
                         </div>
-                        <div class="mb-3">
-                            <label for="prescription" class="form-label">prescription</label>
-                            <input  type="text" class="form-control" id="exampleInputPassword1"/>
+                        <div className="mb-3">
+                            <label htmlFor="prescription" className="form-label"><h5>Prescription</h5></label>
+                            <ReactQuill value={prescription} onChange={(value) => setPrescription(value)} />
                         </div>
-                        <div class="mb-3">
-                            <label for="investigations" class="form-label">investigations</label>
-                            <input type="text" class="form-control" id="exampleInputPassword1"/>
+                        <div className="mb-3">
+                            <label htmlFor="investigations" className="form-label"><h5>Investigations</h5></label>
+                            <ReactQuill value={investigations} onChange={(value) => setInvestigations(value)} />
                         </div>
                         
-                        <button type="submit" class="btn btn-primary myButton">Submit</button>
+                        <button type="submit" className="btn btn-primary myButton">Submit</button>
                     </form>
                 )}
-                
             </div>
             
         </div>
---
+++ frontend/src/components/DoctorView/DashBoardComponents/DashBoardElements/Elements/Appointments/Appointement/PostMedicalNote.js
@@ -0,0 +1,3 @@
+export default function PostMedicalNote(medicalNote){
+    //implement post request
+}
